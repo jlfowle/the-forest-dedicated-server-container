a50e9654d1bba125fd36bb549a61a49f058cdd2d
Jasph Fowler <jasph.fowler@gmail.com>
2022-08-14 10:35:11 -0500

    Initial version

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM steamcmd/steamcmd
+
+USER 0
+
+ENV DEBIAN_FRONTEND=noninteractive \
+  HOME=/opt/app-root \
+  WINEPREFIX=/opt/app-root/winedata/WINE64 \
+  WINEARCH=win64 \
+  DISPLAY=:1.0 \
+  APPDATA=/data \
+  GAMEDIR=/opt/app-root/game
+
+RUN apt-get update && \
+  apt-get install --no-install-recommends -y wine-stable wine32 wine64 xvfb && \
+  apt-get clean autoclean  && \
+  rm -Rf /var/lib/apt/lists/* && \
+  umask 004 && \
+  mkdir -p /opt/app-root $APPDATA && \
+  chown 1001:0 /opt/app-root $APPDATA
+
+USER 1001
+
+COPY entrypoint.sh /entrypoint.sh
+
+RUN umask 004 && \
+  mkdir -p /opt/app-root/winedata/WINE64 && \
+  cd /opt/app-root/winedata && \
+  winecfg && \
+  steamcmd +@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir $GAMEDIR +app_update 556450 validate +quit
+
+WORKDIR /opt/app-root
+
+ENTRYPOINT [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+set -eu
+umask 004
+
+cat >${GAMEDIR}/config.cfg <<EOF
+// Dedicated Server Settings.
+// Server IP address - Note: If you have a router, this address is the internal address, and you need to configure ports forwarding, append the current game port here as well
+serverIP ${SERVER_IP}:${GAME_PORT-27015}
+// Steam Communication Port - Note: If you have a router you will need to open this port.
+serverSteamPort ${STEAM_PORT-8766}
+// Game Communication Port - Note: If you have a router you will need to open this port.
+serverGamePort ${GAME_PORT-27015}
+// Query Communication Port - Note: If you have a router you will need to open this port.
+serverQueryPort ${QUERY_PORT-27016}
+// Server display name
+serverName ${SERVER_NAME-"The Forest Game"}
+// Maximum number of players
+serverPlayers ${SERVER_PLAYERS-8}
+// Server password. blank means no password
+serverPassword ${SERVER_PASSWORD:-""}
+// Server administration password. blank means no password
+serverPasswordAdmin ${SERVER_PASSWORD_ADMIN:-""}
+// Your Steam account name. blank means anonymous (see Steam server account bellow)
+serverSteamAccount ${SERVER_STEAM_ACCOUNT:-""}
+// Enable VAC (Valve Anti Cheat) on the server. off by default, uncomment to enable
+enableVAC ${ENABLE_VAC:-""}
+// Time between server auto saves in minutes
+serverAutoSaveInterval ${SERVER_AUTO_SAVE_INTERVAL-15}
+// Game difficulty mode. Must be set to "Peaceful" "Normal" or "Hard"
+difficulty ${DIFFICULTY-"Normal"}
+// New or continue a game. Must be set to "New" or "Continue"
+initType ${INIT_TYPE-"New"}
+// Slot to save the game. Must be set 1 2 3 4 or 5
+slot ${SLOT-1}
+// Show event log. Must be set "off" or "on"
+showLogs ${SHOW_LOGS-"off"}
+// Contact email for server admin
+serverContact ${SERVER_CONTACT-"email@gmail.com"}
+// No enemies. Must be set to "on" or "off"
+veganMode ${VEGAN_MODE-"off"}
+// No enemies during day time. Must be set to "on" or "off"
+vegetarianMode ${VEGETARIAN_MODE-"off"}
+// Reset all structure holes when loading a save. Must be set to "on" or "off"
+resetHolesMode ${RESET_HOLES_MODE-"off"}
+// Regrow 10% of cut down trees when sleeping. Must be set to "on" or "off"
+treeRegrowMode ${TREE_REGROW_MODE-"off"}
+// Allow building destruction. Must be set to "on" or "off"
+allowBuildingDestruction ${ALLOW_BUILDING_DESTRUCTION-"on"}
+// Allow enemies in creative games. Must be set to "on" or "off"
+allowEnemiesCreativeMode ${ALLOW_ENEMIES_CREATIVE_MODE-"off"}
+// Allow clients to use the built in development console. Must be set to "on" or "off"
+allowCheat ${ALLOW_CHEAT-"off"}
+// Allows defining a custom folder for save slots, leave empty to use the default location
+saveFolderPath ${SAVE_FOLDER_PATH-"${APPDATA}"}
+// Target FPS when no client is connected
+targetFpsIdle ${TARGET_FPS_IDLE-5}
+// Target FPS when there is at least one client connected
+targetFpsActive ${TARGET_FPS_ACTIVE-60}
+EOF
+
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine ${GAMEDIR}/TheForestDedicatedServer.exe -batchmode -savefolderpath "${APPDATA}" -configfilepath "${GAMEDIR}/config.cfg"
